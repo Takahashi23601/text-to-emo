38f1286f0652e18b70c96ae9ac3f34e8f3c7bce8
unknown <tarou23601@gmail.com>
2021-01-31 13:08:19 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from flask import *
+from flask_socketio import SocketIO
+import MeCab
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+import json
+from flask import request, jsonify
+import sys
+
+def Text_Convert(text):
+    x = np.array(0)
+    try:
+        #t = MeCab.Tagger("mecabrc")#デフォルト
+        #t = MeCab.Tagger("-Owakati")#分かち書きのみを出力
+        t = MeCab.Tagger("-Ochasen")#ChaSen 互換形式
+        #t = MeCab.Tagger("-Oyomi")#読みのみを出力
+        nouns = [line.split()[0] for line in t.parse(text).splitlines()
+            if "名詞" or "動詞" in line.split()[-1]]
+        for odd in nouns:
+            if odd != '記号-空白':
+                x = np.hstack([x,odd])
+        return x
+
+    except:
+        print("0.00000\n")
+        sys.exit()
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode=None)
+
+@app.route("/")
+def init():
+    return render_template('Text_To_Emo2.html')
+
+
+@app.route("/input", methods=["POST"])
+def get_form():
+    txt = request.json['data']
+    Text = Text_Convert(txt)
+    count = CountVectorizer()
+    bag = count.fit_transform(Text)
+    term_frequency = np.array(bag.sum(axis=0))[0]
+    df = pd.DataFrame()
+    for i in term_frequency.argsort()[:-1000:-1]:
+        df = df.append([[count.get_feature_names()[i] , term_frequency[i]]],ignore_index = True)
+    df.columns = ['単語','出現回数']
+    df_pn = pd.read_csv('pn_ja.dic',delimiter=":", encoding='cp932',names=["単語","読み","品詞","感情値"])
+    df_pn = df_pn.groupby(["単語","読み","品詞"],as_index=False).first().reset_index(drop=True)
+    df_pn = df_pn.groupby(["単語"],as_index=False).first().reset_index(drop=True)
+    df_emo = pd.merge(df,df_pn, on='単語')
+    df_emo["合計感情値"] = df_emo["出現回数"] * df_emo["感情値"]
+
+    return str(round(df_emo.sum().合計感情値/df_emo.sum().出現回数,5))
+
+if __name__ == "__main__":
+    # debugはデプロイ時にFalseにする
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
---
+++ templates/Text_To_Emo.js
@@ -0,0 +1,128 @@
+  var i = 2 ;
+  var my_div = null;
+  var newh1 = null;
+  var newDiv = null;
+  var num = [];
+ 
+  function equ_result() {
+    var val = 0;
+      for (let x = 1; x < i; x++) {
+        val =  val + num[x];
+      }
+      val = val / (i - 1);
+      document.getElementById("equ").innerHTML = (val).toFixed(5);
+      for (let x = 1; x < i; x++) {
+        document.getElementById("result_" + x + "_2").innerHTML = (num[x] - val).toFixed(5);
+      }
+  }
+
+  function send(x){
+    var str = document.getElementById( "inputform_" + x ).value;
+    $(function(){
+      $('.result_' + x).html("Loading...");
+      document.getElementById("equ").innerHTML = "Loading...";
+      document.getElementById('result_' + x + '_2').innerHTML = "Loading...";
+      $.ajax({
+             url: 'app.py',
+             type: 'POST',
+             scriptCharset: 'utf-8',
+             data: {data:str},
+         }).done(function(data){
+             console.log('感情値：' + data);
+             $('.result_' + x).html(data);
+             num[x] = Number(data);
+             equ_result()
+         }).fail(function(){
+             console.log('failed');
+         });
+     });
+  };
+
+  function addParagraph() {
+    var newDiv = document.createElement("h2");
+    var newContent = document.createTextNode("段落" + i);
+    newDiv.appendChild(newContent);
+
+    var input_data = document.createElement('textarea');
+    input_data.type = 'text';
+    input_data.id = 'inputform_' + i;
+    input_data.placeholder = '段落' + i;
+    input_data.rows="3" 
+    input_data.style="width:70%; float:left;"
+    input_data.onkeydown=function(){
+      textareaResize(event);
+    }
+    input_data.class = "textbox" 
+    input_data.wrap="hard" 
+    var newnum = i;
+    input_data.onkeyup=function(){
+      send(newnum);
+    }     
+
+    var newh = document.createElement("h1");
+    var newContent2 = document.createTextNode("絶対感情値：");
+    newh.appendChild(newContent2);
+    newh.className = "inline";
+
+    var newh1 = document.createElement("h1");
+    var newContent2 = document.createTextNode("入力前");
+    newh1.appendChild(newContent2);
+    newh1.className = 'result_' + i + " feel_point" + " inline";
+
+    var newbr = document.createElement("br");
+
+    var newh_2 = document.createElement("h1");
+    var newContent2_2 = document.createTextNode("相対感情値：");
+    newh_2.appendChild(newContent2_2);
+    newh_2.className = "inline";
+
+    var newh1_2 = document.createElement("h1");
+    var newContent2_2 = document.createTextNode("入力前");
+    newh1_2.appendChild(newContent2_2);
+    newh1_2.className = 'result_' + i + " feel_point" + " inline";
+    newh1_2.id = 'result_' + i + '_2'
+
+    var newbr2 = document.createElement("br");
+    newbr2.clear = "left";
+    
+    var parent = document.getElementById('form_area');
+    parent.appendChild(newDiv);
+    parent.appendChild(input_data);
+    parent.appendChild(newh);
+    parent.appendChild(newh1);
+    parent.appendChild(newbr);
+    parent.appendChild(newh_2);
+    parent.appendChild(newh1_2);
+    parent.appendChild(newbr2);
+    i++ ;
+  }
+
+  function textareaResize(event) {
+    try {
+        elem_id = event.srcElement.id;
+    } catch ( e ) {
+        elem_id = event.target.id;
+    }
+    var keycode = event.keyCode;
+    if (keycode == 13) {
+        var m = document.getElementById(elem_id);
+        var r = m.getAttribute("rows");
+        m.setAttribute("rows", parseInt(r)+1);
+    }
+}
+
+function dispText() {
+  var txt = "";
+  for (let x = 1; x < i; x++) {
+     txt = txt + document.getElementById("inputform_"+x).value + "\n\n";
+  }
+  var blob = new Blob([txt], { "type": "text/plain" });
+
+  //IEの場合
+  if (window.navigator.msSaveBlob) {
+      window.navigator.msSaveBlob(blob, "outFileFromWindows.txt");
+      //IE以外の場合
+  } else {
+      document.getElementById("createFile").href = window.URL.createObjectURL(blob);
+  }
+}
